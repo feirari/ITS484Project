8d2a84bd313ed7da78b20d6b955c6ff1ebb59856
feirari <feirari@users.noreply.github.com>
2016-12-06 21:05:06 +0700

    Add files via upload

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+/**
+ * Load the App component.
+ *  (All the fun stuff happens in "/ReactApp/containers/index.js")
+ *
+ * React Native Starter App
+ * //
+ */
+'use strict';
+
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import AppContainer from './ReactApp/containers/'
+
+AppRegistry.registerComponent('StarterKit', () => AppContainer)
